4b06d4256f3d21bbb56a3359bf684c5ce5b93c2e
CUDDLER <bagldya@gmail.com>
2020-07-21 20:41:18 +0300

    config interfaces typescript and add redux thunk

--- redux/reducers.ts
+++ redux/reducers.ts
@@ -1,26 +1,47 @@
 import { AnyAction } from "redux";
 import { HYDRATE } from "next-redux-wrapper";
+import axios from 'axios'
+
+interface Product {
+    id: number;
+    title: string;
+    img: string;
+    desc: string;
+}
+
+interface Projects {
+    [index: number]: Product
+}
 
 export interface State {
     app: string;
     page: string;
-  }
+    projects: Projects;
+}
   
-  export const reducer = (
-    state: State = { app: "init", page: "init" },
+export const reducer = (
+    state: State = { app: "init", page: "init", projects: [] },
     action: AnyAction
-  ) => {
+) => {
     switch (action.type) {
-      case HYDRATE:
-        if (action.payload.app === "init") delete action.payload.app;
-        if (action.payload.page === "init") delete action.payload.page;
-        return { ...state, ...action.payload };
-      case "APP":
-        return { ...state, app: action.payload };
-      case "PAGE":
-        return { ...state, page: action.payload };
-      default:
-        return state;
+        case HYDRATE:
+            if (action.payload.app === "init") delete action.payload.app;
+            if (action.payload.page === "init") delete action.payload.page;
+            return { ...state, ...action.payload };
+        case "APP":
+            return { ...state, app: action.payload };
+        case "PAGE":
+            return { ...state, page: action.payload };
+        case 'SET_ITEMS':
+            return action.items;
+        default:
+            return state;
     }
-  };
+};
   
+
+export function fetchItems () {
+    return dispatch => axios.get('/api/items')
+        .then(({ data }) => data)
+        .then(items => dispatch({ type: 'SET_ITEMS', items }));
+}
--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from 'redux-thunk'
 import logger from "redux-logger";
 import { MakeStore, createWrapper, Context } from "next-redux-wrapper";
 import { reducer, State } from "./reducers";
 
 export const makeStore: MakeStore<State> = (context: Context) =>
-  createStore(reducer, applyMiddleware(logger));
+  createStore(reducer, applyMiddleware(logger, thunkMiddleware));
 
 export const wrapper = createWrapper<State>(makeStore, { debug: true });
